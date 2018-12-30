aa9519d39f7c2eb98cc4a59017f098b9d4876ce8
pragmatic-form-3566 <chelsiasulin@gmail.com>
2018-12-30 22:44:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ function nowServing (line) {
 function currentLine (line) {
   if (line.length === 0) {
     return 'The line is currently empty.'
-  }
+  } else {
   let finalNamesNumbers = []
   for (let i=0; i<line.length; i++) {
-    
+    finalNamesNumbers.push
   }
+}
 }
